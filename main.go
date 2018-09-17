a297fc123c58b4d8251795932d6e86d1aa706b0d
Michal <karwacki.m@gmail.com>
2018-09-17 14:43:28 +0200

    configJSON support

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -20,17 +21,36 @@ type tokenJSON struct {
 	Expires      expirationTime `json:"expires"`    // broken Facebook spelling of expires_in
 }
 
-func getConfigFromJSON()
-{
+type config struct {
+	GrantType    string `json:"grant_type"`
+	ClientID     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+	Username     string `json:"username"`
+	Password     string `json:"password"`
+}
 
+func getConfigFromJSON(file string) string {
+	var config config
+	configFile, err := os.Open(file)
+	defer configFile.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+
+	payloadSTR := "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"grant_type\"\r\n\r\n" + config.GrantType + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"client_id\"\r\n\r\n" + config.ClientID + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"client_secret\"\r\n\r\n" + config.ClientSecret + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\n" + config.Username + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\n" + config.Password + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+
+	fmt.Println(payloadSTR)
+
+	return payloadSTR
 }
 
 func main() {
 
 	url := "https://svr17.supla.org/oauth/v2/token"
 
-	payload := strings.NewReader("")
-
+	payload := strings.NewReader(getConfigFromJSON("./config.json"))
 	//fmt.Println(payload)
 
 	req, _ := http.NewRequest("POST", url, payload)
